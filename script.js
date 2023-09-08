15fbf827688bbe2e014367eded5ecd7ccea716e4
HenrySaya <henryemnesty@gmail.com>
2023-09-08 16:51:00 +0300

    HNG Task 1

--- script.js
+++ script.js
@@ -1,33 +1,19 @@
-function toggleDarkMode() {
-    document.body.classList.add('dark-mode');
-    document.body.classList.remove('light-mode');
-}
-
-function toggleLightMode() {
-    document.body.classList.add('light-mode');
-    document.body.classList.remove('dark-mode');
-}
-
 function updateCurrentDay() {
     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const currentDate = new Date();
     const currentDayIndex = currentDate.getDay();
     const currentDay = daysOfWeek[currentDayIndex];
-    const currentDayElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
-    currentDayElement.innerText = `Current Day: ${currentDay}`;
+    const currentDayElement = document.querySelector('[data-testid="currentDay"]');
+    currentDayElement.textContent = `${currentDay}`;
 }
 
-// Call this function to update the current day when the page loads 
-
-updateCurrentDay();
-
-// Call this function to update the current day when the page loads
 function updateCurrentUTCTime() {
     const currentUTCTimeInMilliseconds = new Date().getTime();
-    const currentUTCTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
-    currentUTCTimeElement.innerText = `Current UTC Time (milliseconds): ${currentUTCTimeInMilliseconds}`;
+    const currentUTCTimeElement = document.querySelector('[data-testid="currentUtcTime"]');
+    currentUTCTimeElement.textContent = currentUTCTimeInMilliseconds.toString();
 }
 
-// Call this function to update the current UTC time when the page loads
+// Call these functions to update the current day and UTC time when the page loads
+updateCurrentDay();
 updateCurrentUTCTime();
 
