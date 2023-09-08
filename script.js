c63e0cdd61cf22876d401885668ac94bb23fe881
HenrySaya <henryemnesty@gmail.com>
2023-09-08 20:01:55 +0300

    Task Redeployment

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-function updateCurrentDay() {
+function currentDayOfTheWeek() {
     const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const currentDate = new Date();
     const currentDayIndex = currentDate.getDay();
     const currentDay = daysOfWeek[currentDayIndex];
-    const currentDayElement = document.querySelector('[data-testid="currentDay"]');
+    const currentDayElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
     currentDayElement.textContent = `${currentDay}`;
 }
 
 function updateCurrentUTCTime() {
     const currentUTCTimeInMilliseconds = new Date().getTime();
-    const currentUTCTimeElement = document.querySelector('[data-testid="currentUtcTime"]');
+    const currentUTCTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
     currentUTCTimeElement.textContent = currentUTCTimeInMilliseconds.toString();
 }
 
 // Call these functions to update the current day and UTC time when the page loads
-updateCurrentDay();
+currentDayOfTheWeek();
 updateCurrentUTCTime();
 
